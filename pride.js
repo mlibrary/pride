cb201350ea5e15285b31c2a59555fd02970cfc02
Colin Smith Fulton <fultonis@umich.edu>
2015-10-29 14:00:05 -0400

    Update test/example code

--- pride.js
+++ pride.js
@@ -1,3 +1,51 @@
 Pride.init(function() {
   x = Pride.AllDatastores.newSearch('mirlyn');
+  y = new Pride.FieldBooleanNode(
+        'OR',
+        new Pride.FieldBooleanNode(
+          'AND',
+          new Pride.FieldNode(
+            'title',
+            new Pride.ValueBooleanNode(
+              'OR',
+              new Pride.LiteralNode('one'),
+              new Pride.LiteralNode('another number'),
+              new Pride.ValueBooleanNode(
+                'AND',
+                new Pride.LiteralNode('two'),
+                new Pride.GroupNode(
+                  new Pride.LiteralNode('two A'),
+                  new Pride.LiteralNode('two B')
+                ),
+                new Pride.LiteralNode('three')
+              ),
+              new Pride.LiteralNode('blah')
+            )
+          ),
+          new Pride.FieldNode(
+            'author',
+            new Pride.LiteralNode('four')
+          )
+        ),
+        new Pride.FieldBooleanNode(
+          'AND',
+          new Pride.FieldNode(
+            'title',
+            new Pride.LiteralNode('seven')
+          ),
+          new Pride.FieldNode(
+            'author',
+            new Pride.ValueBooleanNode(
+              'OR',
+              new Pride.LiteralNode('ten')
+            )
+          )
+        )
+      );
+  z = new Pride.ValueBooleanNode(
+        'AND',
+        new Pride.GroupNode(
+          new Pride.LiteralNode('word')
+        )
+      )
 });
