641822125e57840c1d34c98414f12f5dee2de932
Colin Smith Fulton <fultonis@umich.edu>
2015-11-03 17:32:14 -0500

    Set message data using a hash to clean things up a bit

--- singletons/messenger.js
+++ singletons/messenger.js
@@ -1,22 +1,26 @@
 var Pride = Pride || {};
 
 Pride.Messenger = new Pride.Observable(function() {
-  this.sendMessage = function(a_summary, a_class, some_details) {
-    this.notifyObservers({
-      'class':   a_class      || 'info',
-      'summary': a_summary    || '',
-      'details': some_details || ''
-    });
+  this.sendMessage = function(message) {
+    if (message['summary']) {
+      message['class']   = message['class']   || 'info';
+      message['details'] = message['details'] || '';
+
+      this.notifyObservers(message);
+
+      console.log('[messenger] MESSAGE SENT:', message)
+    }
 
     return this;
   };
 
   this.sendMessageArray = function(message_array) {
-    messenger = this;
+    var messenger = this;
 
-    _.each(message_array, function(message) {
-      messenger.notifyObservers(message);
-    });
+    _.each(
+      message_array,
+      function(message) { messenger.sendMessage(message); }
+    );
 
     return this;
   };
