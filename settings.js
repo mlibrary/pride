55b23b6b412e06d4dac8317c31b9d75310623a2e
Colin Smith Fulton <fultonis@umich.edu>
2015-11-05 15:18:54 -0500

    Add comments about what all the settings do

--- settings.js
+++ settings.js
@@ -1,14 +1,29 @@
 var Pride = Pride || {};
 
 Pride.settings = {
+  // default_cache_size: If a cache size isn't set for a datastore, this value
+  //                     is used instead
+  // cache_size:         Key-value pairs where each key is the UID of a
+  //                     datastore, and the value gives the cache size for that
+  //                     particular datastore.
   default_cache_size: 100,
   cache_size: {
 
   },
 
+  // datastore_url: URL from which Pride can get all the possible datastores.
+
+  // connection_attempts: How many times Pride will attempt an HTTP request
+  //                      before giving up.
+  // ms_between_attempts: How long Pride will wait to try another HTTP request
+  //                      after one fails.
   connection_attempts: 5,
   ms_between_attempts: 1500,
 
+  // message_formats: Key-value pairs where each key is the ID of a message type
+  //                  and the value is what that message should say. A dollar
+  //                  sign preceded by a number will be replaced when the
+  //                  message is created.
   message_formats: {
     failed_record_load: 'Failed to load $1',
     failed_search_run:  'Failed to search $1'
