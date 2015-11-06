f70dafb97a7a0b42559ea7c51401e845b67f8394
Colin Smith Fulton <fultonis@umich.edu>
2015-11-06 18:12:15 -0500

    Move Pride.AllDataStores.update() logic into Pride.init() and make it so Pride.init() will not rehit the server after a success

--- functions/init.js
+++ functions/init.js
@@ -1,10 +1,32 @@
 var Pride = Pride || {};
 
 Pride.init = function(on_success, on_failure, attempts) {
-  Pride.AllDatastores.update({
-    success:         on_success,
-    failure:         on_failure,
-    attempts:        attempts,
-    failure_message: 'Failed to load datastores'
-  });
+  on_success = on_success || function() {};
+
+  if (this.already_initialized) {
+    on_success();
+  } else {
+    var self = this;
+
+    Pride.request({
+      url:             Pride.settings.datastores_url,
+      attempts:        attempts,
+      failure:         on_failure,
+      failure_message: 'Failed to load datastores',
+      success: function(response) {
+        self.already_initialized = true;
+
+        Pride.AllDatastores.array = _.map(
+          response['response'],
+          function(datastore_data) {
+            return new Pride.Datastore(datastore_data);
+          }
+        );
+
+        on_success();
+      }
+    });
+  }
 };
+
+Pride.init.already_initialized = false;
--- singletons/all_datastores.js
+++ singletons/all_datastores.js
@@ -16,25 +16,6 @@ Pride.AllDatastores = {
     _.each(this.array, func);
 
     return this;
-  },
-
-  update: function(args) {
-    self = this;
-
-    Pride.request({
-      url: Pride.settings.datastores_url,
-      failure: args.failure,
-      success: function(response) {
-        self.array = _.map(
-                       response['response'],
-                       function(datastore_data) {
-                         return new Pride.Datastore(datastore_data);
-                       }
-                     );
-
-        if (_.isFunction(args.success)) args.success();
-      }
-    });
   }
 };
 
