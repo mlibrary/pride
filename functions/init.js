2bd00072733a984f5a9dabbd02dbdd12c6c17167
Colin Smith Fulton <fultonis@umich.edu>
2015-11-20 14:34:15 -0500

    Allow Pride.init() to be called multiple times safely

--- functions/init.js
+++ functions/init.js
@@ -6,32 +6,51 @@
 var Pride = Pride || {};
 
 Pride.init = function(on_success, on_failure, attempts) {
-  on_success = on_success || function() {};
+  var self = Pride.init;
 
-  if (this.already_initialized) {
+  if (self.is_initialized) {
     on_success();
+  } else if (self.failed_init) {
+    on_failure();
   } else {
-    var self = this;
+    if (_.isFunction(on_success)) self.success_functions.push(on_success);
+    if (_.isFunction(on_failure)) self.failure_functions.push(on_failure);
 
-    Pride.request({
-      url:             Pride.settings.datastores_url,
-      attempts:        attempts,
-      failure:         on_failure,
-      failure_message: 'Failed to load datastores',
-      success: function(response) {
-        self.already_initialized = true;
+    if (!self.running_request) {
+      self.running_request = true;
 
-        Pride.AllDatastores.array = _.map(
-          response['response'],
-          function(datastore_data) {
-            return new Pride.Datastore(datastore_data);
-          }
-        );
+      Pride.request({
+        url:             Pride.settings.datastores_url,
+        attempts:        attempts,
+        failure_message: 'Failed to load datastores',
 
-        on_success();
-      }
-    });
+        failure: function() {
+          self.failed_init = true;
+          self.failure_functions.call_all();
+        },
+        success: function(response) {
+          self.is_initialized = true;
+
+          Pride.AllDatastores.array = _.map(
+            response['response'],
+            function(datastore_data) {
+              return new Pride.Datastore(datastore_data);
+            }
+          );
+
+          self.success_functions.call_all();
+        }
+      });
+    }
   }
 };
 
-Pride.init.already_initialized = false;
+Pride.init.success_functions = [];
+Pride.init.failure_functions = [];
+
+Pride.init.failure_functions.call_all = Pride.init.success_functions.call_all =
+  function(func_array) {
+    _.each(this, function(func) { func(); });
+    this.length = 0;
+    return this;
+  };
