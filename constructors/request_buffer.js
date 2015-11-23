f11dc47390aaa1cf47b66111e3447210d6a30864
Colin Smith Fulton <fultonis@umich.edu>
2015-11-23 17:23:40 -0500

    Add and use Pride.RequestBuffer to abstract away request callback buffering

---
+++ constructors/request_buffer.js
@@ -0,0 +1,80 @@
+// Copyright (c) 2015, Regents of the University of Michigan.
+// All rights reserved. See LICENSE.txt for details.
+
+// Authored by Colin Fulton (fultonis@umich.edu)
+
+var Pride = Pride || {};
+
+Pride.RequestBuffer = function(request_options) {
+  var success_functions = [];
+  var failure_functions = [];
+
+  var request_issued     = false;
+  var request_successful = false;
+  var request_failed     = false;
+
+  var cached_response_data;
+
+  this.request = function(func_hash) {
+    success_functions.push(func_hash.success);
+    failure_functions.push(func_hash.failure);
+
+    if (request_issued) {
+      callWithResponse();
+    } else {
+      sendRequest();
+    }
+  };
+
+  var callWithResponse = function(data) {
+    response_data = cached_response_data || data;
+
+    if (request_successful) {
+      callAll(success_functions);
+    } else if (request_failed) {
+      callAll(failure_functions);
+    }
+  };
+
+  var sendRequest = function() {
+    request_issued = true;
+
+    Pride.request({
+      url:             request_options.url,
+      attempts:        request_options.attempts || Pride.settings.connection_attempts,
+      failure_message: request_options.failure_message,
+
+      failure: function(error) {
+        request_failed = true;
+
+        Pride.safeCall(request_options.before_failure, response);
+
+        callWithResponse(error);
+
+        Pride.safeCall(request_options.after_failure, response);
+      },
+      success: function(response) {
+        request_successful = true;
+
+        Pride.safeCall(request_options.before_success, response);
+
+        if (_.isFunction(request_options.edit_response)) {
+          response = request_options.edit_response(response);
+        }
+
+        callWithResponse(response);
+
+        Pride.safeCall(request_options.after_success, response);
+      }
+    });
+  };
+
+  var callAll = function(func_array) {
+    _.each(func_array, function(func) {
+      Pride.safeCall(func, cached_response_data);
+    });
+
+    success_functions.length = 0;
+    failure_functions.length = 0;
+  };
+};
--- functions/init.js
+++ functions/init.js
@@ -5,54 +5,19 @@
 
 var Pride = Pride || {};
 
-Pride.init = function(options) {
-  var self         = Pride.init;
-  var success_func = options.success;
-  var failure_func = options.failure;
+Pride.init_request_buffer = new Pride.RequestBuffer({
+                              url:             Pride.settings.datastores_url,
+                              attempts:        Pride.settings.init_attempts,
+                              failure_message: 'Failed to load datastores',
+                              edit_response:   function() { return undefined; },
+                              before_success:  function(response) {
+                                Pride.AllDatastores.array = _.map(
+                                  response['response'],
+                                  function(datastore_data) {
+                                    return new Pride.Datastore(datastore_data);
+                                  }
+                                );
+                              }
+                            });
 
-  if (self.is_initialized) {
-    success_func();
-  } else if (self.failed_init) {
-    failure_func();
-  } else {
-    if (_.isFunction(success_func)) self.success_functions.push(success_func);
-    if (_.isFunction(failure_func)) self.failure_functions.push(failure_func);
-
-    if (!self.running_request) {
-      self.running_request = true;
-
-      Pride.request({
-        url:             Pride.settings.datastores_url,
-        attempts:        Pride.settings.init_attempts,
-        failure_message: 'Failed to load datastores',
-
-        failure: function() {
-          self.failed_init = true;
-          self.failure_functions.call_all();
-        },
-        success: function(response) {
-          self.is_initialized = true;
-
-          Pride.AllDatastores.array = _.map(
-            response['response'],
-            function(datastore_data) {
-              return new Pride.Datastore(datastore_data);
-            }
-          );
-
-          self.success_functions.call_all();
-        }
-      });
-    }
-  }
-};
-
-Pride.init.success_functions = [];
-Pride.init.failure_functions = [];
-
-Pride.init.failure_functions.call_all = Pride.init.success_functions.call_all =
-  function(func_array) {
-    _.each(this, function(func) { func(); });
-    this.length = 0;
-    return this;
-  };
+Pride.init = Pride.init_request_buffer.request;
