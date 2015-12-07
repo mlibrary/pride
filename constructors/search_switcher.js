a3118dc0a528ea32b8b85a073e0d5b01165216ca
Colin Smith Fulton <fultonis@umich.edu>
2015-12-07 15:46:23 -0500

    Create Pride.utils.SearchSwitcher

---
+++ constructors/search_switcher.js
@@ -0,0 +1,53 @@
+// Copyright (c) 2015, Regents of the University of Michigan.
+// All rights reserved. See LICENSE.txt for details.
+
+// Authored by Colin Fulton (fultonis@umich.edu)
+
+Pride.utils.SearchSwitcher = function(current_search, cached_searches) {
+  var self         = this;
+  var search_cache = new Pride.core.MultiSearch({}, true, cached_searches);
+
+  current_search.setMute(false);
+
+  this.run = function(cache_size) {
+    current_search.run(cache_size);
+    search_cache.run(0);
+
+    return self;
+  };
+
+  this.set = function(settings) {
+    current_search.set(settings);
+    search_cache.set(settings);
+
+    return self;
+  };
+
+  this.switchTo = function(requested_uid) {
+    if (requested_uid != current_search) {
+      current_search.setMute(true);
+      console.log(search_cache)
+      console.log(search_cache.searches)
+      search_cache.searches.push(current_search);
+      current_search = undefined;
+
+      search_cache.searches = _.reject(
+                                  search_cache.searches,
+                                  function(search) {
+                                    if (search.uid == requested_uid) {
+                                      current_search = search;
+                                      return true;
+                                    }
+                                  }
+                                );
+
+      if (!current_search) {
+        throw 'Could not find a search with a UID of: ' + requested_uid;
+      }
+
+      current_search.setMute(false);
+    }
+
+    return self;
+  };
+};
