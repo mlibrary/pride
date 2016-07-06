6a9f6b54ec4197238e03d0e7579cd27c1b5f4c68
Colin Fulton <fultonis@umich.edu>
2016-07-06 11:27:00 -0400

    Create a way to mock out requests

---
+++ spec/support/mock_request.js
@@ -0,0 +1,51 @@
+// Copyright (c) 2015, Regents of the University of Michigan.
+// All rights reserved. See LICENSE.txt for details.
+
+// Authored by Colin Fulton (fultonis@umich.edu)
+
+if (typeof reqwest !== 'undefined') var originalRequest = reqwest;
+
+function testRequest(method, request_info) {
+  describe('sends a valid request', function() {
+    it('is given the nessesary fields', function() {
+      _.each(['url', 'data', 'method', 'contentType'], function(field) {
+        expect(typeOf(request_info[field])).toEqual('string');
+      });
+
+      expect(typeOf(request_info['url'])).toEqual('string');
+      expect(typeOf(request_info['method'])).toEqual('string');
+      expect(typeOf(request_info['content_type'])).toEqual('string');
+    });
+
+    it('is given the expected method', function() {
+      expect(request_info['method']).toEqual(method);
+    });
+
+    if (request_info['type'] == 'json') {
+      describe('when type is "json"', function() {
+        it('expects contentType to be application/json', function() {
+          expect(request_info['contentType']).toEqual('application/json');
+        });
+
+        it('gives a valid JSON encoded string', function() {
+          expect(JSON.parse(request_info['data'])).not.toThrow();
+        });
+      });
+    }
+  });
+}
+
+function mockRequestSuccess(method, result) {
+  reqwest = function(request_info) {
+    testRequest(method, request_info);
+    request_info['success'](result);
+  };
+}
+
+
+function mockRequestError(method, result) {
+  reqwest = function(request_info) {
+    testRequest(method, request_info);
+    request_info['error'](result);
+  };
+}
