2e4b714428c3354ed919debf0aa2069c567f1f9f
Albert Bertram <bertrama@umich.edu>
2018-12-20 14:21:46 -0500

    SEARCH-796 Fixes for the front-end record matching.

--- source/singletons/preference_engine.js
+++ source/singletons/preference_engine.js
@@ -75,12 +75,16 @@ Pride.PreferenceEngine = {
         datastore = 'articlesplus';
       }
       else if ((remove = record.tags.indexOf('databases-favorite')) >= 0) {
-        id = record.id[0];
+        id = record.id[0].split('/')[4];
         datastore = 'databases';
       }
       else if ((remove = record.tags.indexOf('journals-favorite')) >= 0) {
-        id = record.id[0];
+        id = record.id[0].split('/')[4];
         datastore = 'journals';
+      }
+      else if ((remove = record.tags.indexOf('website-favorite')) >= 0) {
+        id = record.id[0];
+        datastore = 'website';
       }
       else {
         return this;
