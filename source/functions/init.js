3eaa25cef73fc18a4ad9c07a97d635e0f9d65f5d
Albert Bertram <bertrama@umich.edu>
2019-04-19 12:59:00 -0400

    Update init.js

--- source/functions/init.js
+++ source/functions/init.js
@@ -5,20 +5,22 @@
 
 import { _ } from 'underscore';
 
+Pride.initWith = function(data) {
+  // TODO: Look for a better place for this later.
+  Pride.Settings.default_institution = data.default_institution;
+  Pride.AllDatastores.array = _.map(
+    data.response,
+    function(datastore_data) {
+      return new Pride.Core.Datastore(datastore_data);
+    }
+  );
+};
+
 Pride.init = new Pride.Util.RequestBuffer({
   url:             function() { return Pride.Settings.datastores_url; },
   attempts:        function() { return Pride.Settings.init_attempts; },
   failure_message: function() { return Pride.Messenger.preset('failed_init'); },
 
   edit_response:   function() { return undefined; },
-  before_success:  function(data) {
-    // TODO: Look for a better place for this later.
-    Pride.Settings.default_institution = data.default_institution;
-    Pride.AllDatastores.array = _.map(
-      data.response,
-      function(datastore_data) {
-        return new Pride.Core.Datastore(datastore_data);
-      }
-    );
-  }
+  before_success: this.initWith  
 }).request;
