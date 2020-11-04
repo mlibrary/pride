8b8db477ee56779acc8c5e5a3b3cc2d8a3964699
Albert Bertram <bertrama@umich.edu>
2020-11-04 14:05:39 -0500

    Hooking up Pride.Messenger.

--- pride.execjs.js
+++ pride.execjs.js
@@ -1930,6 +1930,8 @@ Pride.AllDatastores = {
 
 // Authored by Colin Fulton (fultonis@umich.edu)
 
+
+
 Pride.Messenger = new Pride.Util.FuncBuffer(function() {
   var notifyObservers = this.call;
 
@@ -1943,27 +1945,27 @@ Pride.Messenger = new Pride.Util.FuncBuffer(function() {
   this.clear  = undefined;
 
   this.sendMessage = function(message) {
-    // if (message['summary']) {
-    //   message['class']   = message['class']   || 'info';
-    //   message['details'] = message['details'] || '';
+    if (message.summary) {
+      message.class   = message.class   || 'info';
+      message.details = message.details || '';
 
-    //   notifyObservers(message);
+      notifyObservers(message.class, message);
 
-    //   Pride.Core.log('Messenger', 'MESSAGE SENT', message);
-    // }
+      Pride.Core.log('Messenger', 'MESSAGE SENT', message);
+    }
 
-    // return this;
+    return this;
   };
 
   this.sendMessageArray = function(message_array) {
-    // var messenger = this;
+    var messenger = this;
 
-    // _.each(
-    //   message_array,
-    //   function(message) { messenger.sendMessage(message); }
-    // );
+    _.each(
+      message_array,
+      function(message) { messenger.sendMessage(message); }
+    );
 
-    // return this;
+    return this;
   };
 
   // Given a type of preset message and some optional arguments, generate a
--- pride.js
+++ pride.js
@@ -1808,12 +1808,9 @@ Pride.AllDatastores = {
 // All rights reserved. See LICENSE.txt for details.
 
 // Authored by Colin Fulton (fultonis@umich.edu)
-"use strict";
+'use strict';
 
-// Copyright (c) 2015, Regents of the University of Michigan.
-// All rights reserved. See LICENSE.txt for details.
-
-// Authored by Colin Fulton (fultonis@umich.edu)
+var _underscore = require('underscore');
 
 Pride.Messenger = new Pride.Util.FuncBuffer(function () {
   var notifyObservers = this.call;
@@ -1828,27 +1825,26 @@ Pride.Messenger = new Pride.Util.FuncBuffer(function () {
   this.clear = undefined;
 
   this.sendMessage = function (message) {
-    // if (message['summary']) {
-    //   message['class']   = message['class']   || 'info';
-    //   message['details'] = message['details'] || '';
+    if (message.summary) {
+      message.class = message.class || 'info';
+      message.details = message.details || '';
 
-    //   notifyObservers(message);
+      notifyObservers(message.class, message);
 
-    //   Pride.Core.log('Messenger', 'MESSAGE SENT', message);
-    // }
+      Pride.Core.log('Messenger', 'MESSAGE SENT', message);
+    }
 
-    // return this;
+    return this;
   };
 
   this.sendMessageArray = function (message_array) {
-    // var messenger = this;
+    var messenger = this;
 
-    // _.each(
-    //   message_array,
-    //   function(message) { messenger.sendMessage(message); }
-    // );
+    _underscore._.each(message_array, function (message) {
+      messenger.sendMessage(message);
+    });
 
-    // return this;
+    return this;
   };
 
   // Given a type of preset message and some optional arguments, generate a
@@ -1866,7 +1862,10 @@ Pride.Messenger = new Pride.Util.FuncBuffer(function () {
     //             )
     //             .replace('\\$', '$');
   };
-});
+}); // Copyright (c) 2015, Regents of the University of Michigan.
+// All rights reserved. See LICENSE.txt for details.
+
+// Authored by Colin Fulton (fultonis@umich.edu)
 "use strict";
 
 Pride.Parser =
