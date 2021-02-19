e13a5e5933930fd942370ba58d51b2562d0b6ebc
Erin E. Sullivan <sulliv.erin@gmail.com>
2021-02-19 15:16:15 -0500

    Pride.Util.slice() function and unit test

--- src/Pride/Util.js
+++ src/Pride/Util.js
@@ -1,11 +1,13 @@
 import deepClone from './Util/deepClone';
 import escape from './Util/escape';
 import isDeepMatch from './Util/isDeepMatch';
+import slice from './Util/slice';
 
 const Util = {
   deepClone: (original) => deepClone(original),
   escape: (string) => escape(string),
-  isDeepMatch: (object, pattern) => isDeepMatch(object, pattern)
+  isDeepMatch: (object, pattern) => isDeepMatch(object, pattern),
+  slice: (array, begin, end) => slice(array, begin, end)
 };
 
 export default Util;
---
+++ src/Pride/Util/slice.test.js
@@ -0,0 +1,34 @@
+import { expect } from 'chai';
+import slice from './slice';
+
+describe('slice()', () => {
+  it('returns an array', () => {
+    expect(slice([1, 2, 3], 1)).to.be.an('array');
+  });
+
+  describe('slicing arrays', () => {
+    it('slices from start', () => {
+      expect(slice([1, 2, 3], 1)).to.deep.equal([2, 3]);
+    });
+
+    it('slices to end', () => {
+      expect(slice([1, 2, 3, 4], 1, 3)).to.deep.equal([2, 3]);
+    });
+  });
+
+  describe('slicing array like objects', () => {
+    beforeEach(() => {
+      this.arrayLike = (function() {
+        return arguments;
+      }(1, 2, 3, 4));
+    });
+
+    it('slices from start', () => {
+      expect(slice(this.arrayLike, 1)).to.deep.equal([2, 3, 4]);
+    });
+
+    it('slices to end', () => {
+      expect(slice(this.arrayLike, 1, 3)).to.deep.equal([2, 3]);
+    });
+  });
+});
