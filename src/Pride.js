227e5e515bd706a94e4229e4eaf23a1579876d53
Erin E. Sullivan <sulliv.erin@gmail.com>
2021-02-23 15:29:09 -0500

    Adding Paginater to Pride

--- src/Pride.js
+++ src/Pride.js
@@ -1,9 +1,11 @@
 'use strict';
 
 import Util from './Pride/Util.js';
+import Paginater from './Pride/Util/Paginater';
 
 const Pride = {};
 
 Object.defineProperty(Pride, 'Util', { value: Util });
+Object.defineProperty(Pride.Util, 'Paginater', { value: Paginater });
 
 export default Pride;
