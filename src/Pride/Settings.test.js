32a4a5e83422d8d27c369ce1d9775a330a85501c
Erin E. Sullivan <sulliv.erin@gmail.com>
2021-03-05 11:12:49 -0500

    Initialize Pride.Settings()

---
+++ src/Pride/Settings.test.js
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+import Settings from './Settings';
+
+describe.only('Settings()', () => {
+  it('does a thing', () => {
+    console.log(Settings);
+    expect(Settings).to.not.be.null;
+  });
+});
