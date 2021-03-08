2fd2f63266ebdf0ed894a89ae14d213968cccbff
Erin E. Sullivan <sulliv.erin@gmail.com>
2021-03-08 12:37:51 -0500

    Initialize Pride.Core.Query

--- src/Pride/Core.js
+++ src/Pride/Core.js
@@ -1,7 +1,10 @@
 import nodeFactory from './Core/nodeFactory';
+import Query from './Core/Query';
 
 const Core = {
   nodeFactory: (type, childTypes, extention) => nodeFactory(type, childTypes, extention)
 };
+
+Object.defineProperty(Core, 'Query', { value: Query });
 
 export default Core;
---
+++ src/Pride/Core/Query.test.js
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+import Query from './Query';
+
+describe.only('Query()', () => {
+  it('does a thing', () => {
+    console.log(Query);
+    expect(Query).to.not.be.null;
+  });
+});
