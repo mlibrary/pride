4548e0f29d9710569093f404058d16d0e6ecddbd
Erin E. Sullivan <sulliv.erin@gmail.com>
2021-03-08 09:45:13 -0500

    Initialize Pride.Core.Record

---
+++ src/Pride/Core/Record.test.js
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+import Record from './Record';
+
+describe.only('Record()', () => {
+  it('does a thing', () => {
+    console.log(Record);
+    expect(Record).to.not.be.null;
+  });
+});
