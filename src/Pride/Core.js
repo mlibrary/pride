166063e641d7d976f4ad301e6d535eb0660a216d
Erin E. Sullivan <sulliv.erin@gmail.com>
2021-03-08 10:55:05 -0500

    Initialize Pride.Core.SearchBase

--- src/Pride/Core.js
+++ src/Pride/Core.js
@@ -1,7 +1,10 @@
 import nodeFactory from './Core/nodeFactory';
+import SearchBase from './SearchBase';
 
 const Core = {
   nodeFactory: (type, childTypes, extention) => nodeFactory(type, childTypes, extention)
 };
+
+Object.defineProperty(Core, 'SearchBase', { value: SearchBase });
 
 export default Core;
---
+++ src/Pride/Core/SearchBase.test.js
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+import SearchBase from './SearchBase';
+
+describe.only('SearchBase()', () => {
+  it('does a thing', () => {
+    console.log(SearchBase);
+    expect(SearchBase).to.not.be.null;
+  });
+});
