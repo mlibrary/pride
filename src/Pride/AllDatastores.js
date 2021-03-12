5b0b382cb02aead85859727d53f4074c01842d40
Erin E. Sullivan <sulliv.erin@gmail.com>
2021-03-12 15:26:45 -0500

    Simplifying Pride.AllDatastores

--- src/Pride/AllDatastores.js
+++ src/Pride/AllDatastores.js
@@ -6,26 +6,20 @@ const AllDatastores = {
   get: function(uid) {
     return _.find(
       this.array,
-      function(datastore) {
-        return datastore.get('uid') === uid;
-      }
+      (datastore) => datastore.get('uid') === uid
     );
   },
 
   newSearch: function(uid) {
     const datastore = _.find(
       this.array,
-      function(datastore) {
-        return datastore.get('uid') === uid;
-      }
+      (datastore) => datastore.get('uid') === uid
     );
-
     return datastore ? datastore.baseSearch() : undefined;
   },
 
   each: function(func) {
     _.each(this.array, func);
-
     return this;
   }
 };
