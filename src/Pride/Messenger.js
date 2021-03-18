75bc3610cc096524fd566c84217fa31f8abaacb5
Erin E. Sullivan <sulliv.erin@gmail.com>
2021-03-18 12:47:05 -0400

    Simplifying Pride.Messenger

--- src/Pride/Messenger.js
+++ src/Pride/Messenger.js
@@ -1,71 +1,56 @@
-/*
- * import FuncBuffer from './Util/FuncBuffer';
- * import log from './Core/log';
- * import Settings from './Settings';
- * import slice from './Util/slice';
- */
+import { _ } from 'underscore';
+import FuncBuffer from './Util/FuncBuffer';
+import log from './Core/log';
+import Settings from './Settings';
+import slice from './Util/slice';
 
-const Messenger = function() { // const Messenger = new FuncBuffer(function() {
+const Messenger = new FuncBuffer(function() {
   const notifyObservers = this.call;
-
-  this.addObserver = this.add;
-  this.removeObserver = this.remove;
-  this.clearObservers = this.clear;
 
   this.call = undefined;
   this.add = undefined;
   this.remove = undefined;
   this.clear = undefined;
 
-  this.sendMessage = function(message) {
-    /*
-     * if (message['summary']) {
-     *   message['class']   = message['class']   || 'info';
-     *   message['details'] = message['details'] || '';
-     */
+  this.sendMessage = (message) => {
+    if (message.summary) {
+      message.class = message.class || 'info';
+      message.details = message.details || '';
 
-    //   notifyObservers(message);
+      notifyObservers(message.class, message);
 
-    /*
-     *   Pride.Core.log('Messenger', 'MESSAGE SENT', message);
-     * }
-     */
+      log('Messenger', 'MESSAGE SENT', message);
+    }
 
-    // return this;
+    return this;
   };
 
-  this.sendMessageArray = function(messageArray) {
-    // var messenger = this;
+  this.sendMessageArray = (messageArray) => {
+    _.each(
+      messageArray,
+      (message) => this.sendMessage(message)
+    );
 
-    /*
-     * _.each(
-     *   messageArray,
-     *   function(message) { messenger.sendMessage(message); }
-     * );
-     */
-
-    // return this;
+    return this;
   };
 
   /*
    * Given a type of preset message and some optional arguments, generate a
    * message string.
    */
-  this.preset = function(type) {
-    // var variables = Pride.Util.slice(arguments);
-
+  this.preset = (type) => {
     /*
-     * return Pride.Settings
-     *             .message_formats[type]
-     *             .replace(
-     *               /(^|[^\\])\$(\d+)/,
-     *               function(match, previousChar, number) {
-     *                 return previousChar + (variables[Number(number)] || '');
-     *               }
-     *             )
-     *             .replace('\\$', '$');
+     *const variables = slice(arguments);
+     *
+     *return Settings
+     *  .message_formats[type]
+     *  .replace(
+     *    /(^|[^\\])\$(\d+)/,
+     *    (match, previousChar, number) => previousChar + (variables[Number(number)] || '')
+     *  )
+     *  .replace('\\$', '$');
      */
   };
-}; // });
+});
 
 export default Messenger;
