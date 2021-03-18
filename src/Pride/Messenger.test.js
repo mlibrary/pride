feee1f6d53e1c913848c1a419af164531c6ecb42
Erin E. Sullivan <sulliv.erin@gmail.com>
2021-03-18 15:54:03 -0400

    Pushing unit test progress.

--- src/Pride/Messenger.test.js
+++ src/Pride/Messenger.test.js
@@ -2,8 +2,43 @@ import { expect } from 'chai';
 import Messenger from './Messenger';
 
 describe.only('Messenger()', () => {
-  it('does a thing', () => {
-    console.log(Messenger);
-    expect(Messenger).to.not.be.null;
+  describe('sendMessage()', () => {
+    beforeEach(function() {
+      this.sendMessage = Messenger.sendMessage({
+        summary: 'There was an error.',
+        class: 'error'
+      });
+    });
+    it('sendMessage', () => {
+      console.log(this.sendMessage);
+      expect(this.sendMessage).to.not.be.null;
+    });
+  });
+  describe('sendMessageArray()', () => {
+    beforeEach(function() {
+      this.sendMessageArray = Messenger.sendMessageArray([
+        {
+          summary: 'There was an error.',
+          class: 'error'
+        },
+        {
+          summary: 'This has been a success!',
+          class: 'success'
+        }
+      ]);
+    });
+    it('sendMessageArray', () => {
+      console.log(this.sendMessageArray);
+      expect(this.sendMessageArray).to.not.be.null;
+    });
+  });
+  describe('preset()', () => {
+    beforeEach(function() {
+      this.preset = Messenger.preset('failed_init');
+    });
+    it('preset', () => {
+      console.log(this.preset);
+      expect(this.preset).to.not.be.null;
+    });
   });
 });
