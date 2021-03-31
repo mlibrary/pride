e14d2bcf43918adf95a157c009e2fd79ef98e8ef
Erin E. Sullivan <sulliv.erin@gmail.com>
2021-03-31 13:30:28 -0400

    Simplifying and adding unit test

--- src/Pride/Util/RequestBuffer.js
+++ src/Pride/Util/RequestBuffer.js
@@ -1,10 +1,8 @@
 import { _ } from 'underscore';
-/*
- * import FuncBuffer from './FuncBuffer';
- * import request from './request';
- * import safeCall from './safeCall';
- * import Settings from '../Settings';
- */
+import FuncBuffer from './FuncBuffer';
+import request from './request';
+import safeCall from './safeCall';
+import Settings from '../Settings';
 
 const RequestBuffer = function(requestOptions) {
   requestOptions = requestOptions || {};
@@ -14,11 +12,11 @@ const RequestBuffer = function(requestOptions) {
   let requestIssued = false;
   let requestSuccessful = false;
   let requestFailed = false;
-
   let cachedResponseData;
 
   this.request = function(funcHash) {
-    funcBuffer.add(funcHash.success, 'success')
+    funcBuffer
+      .add(funcHash.success, 'success')
       .add(funcHash.failure, 'failure');
 
     if (requestIssued) {
@@ -30,53 +28,43 @@ const RequestBuffer = function(requestOptions) {
     return this;
   };
 
-  const callWithResponse = function(data) {
+  const callWithResponse = (data) => {
     cachedResponseData = data || cachedResponseData;
 
-    if (requestSuccessful) {
-      callThenClear('success');
-    } else if (requestFailed) {
-      callThenClear('failure');
-    }
+    if (requestSuccessful) callThenClear('success');
+    if (requestFailed) callThenClear('failure');
   };
 
-  const sendRequest = function() {
+  const sendRequest = () => {
     requestIssued = true;
 
     request({
       url: safeCall(requestOptions.url),
-      attempts: safeCall(requestOptions.attempts) ||
-                       Settings.connection_attempts,
+      attempts: safeCall(requestOptions.attempts) || Settings.connection_attempts,
       failure_message: safeCall(requestOptions.failure_message),
 
-      failure: function(error) {
+      failure: (error) => {
         requestFailed = true;
-
         safeCall(requestOptions.before_failure, error);
-
         callWithResponse(error);
-
         safeCall(requestOptions.after_failure, error);
       },
 
-      success: function(response) {
+      success: (response) => {
         requestSuccessful = true;
-
         safeCall(requestOptions.before_success, response);
-
         if (_.isFunction(requestOptions.edit_response)) {
           response = requestOptions.edit_response(response);
         }
-
         callWithResponse(response);
-
         safeCall(requestOptions.after_success, response);
       }
     });
   };
 
-  const callThenClear = function(name) {
-    funcBuffer.call(name, cachedResponseData)
+  const callThenClear = (name) => {
+    funcBuffer
+      .call(name, cachedResponseData)
       .clearAll();
   };
 };
