8b8b5e2dbf8cc686079bf28855f9262e0dcc5a94
Erin E. Sullivan <sulliv.erin@gmail.com>
2021-04-16 11:28:17 -0400

    Get holdings data directly instead of through URL

--- source/constructors/holdings.js
+++ source/constructors/holdings.js
@@ -1,51 +1,23 @@
-// Copyright (c) 2017, Regents of the University of Michigan.
-// All rights reserved. See LICENSE.txt for details.
+/*
+ * Copyright (c) 2021, Regents of the University of Michigan.
+ * All rights reserved. See LICENSE.txt for details.
+ */
 
 // Authored by Albert Bertram (bertrama@umich.edu)
-
-import { _ } from 'underscore';
 
 Pride.Core.Holdings = function(data) {
   this.data = data;
 
-  var getHoldingsUrl = function(data) {
-    var ret;
-    _.each(data.fields, function(field) {
-      if (field.uid === 'holdings_url') {
-        ret = field.value;
-      }
-    });
-    return ret;
+  const getResourceAccess = function(data) {
+    const dataField = data.fields.find((field) => field.uid === 'resource_access');
+    return dataField.value;
   };
 
-  var getResourceAccess = function(data) {
-    var ret;
-    _.each(data.fields, function(field) {
-      if (field.uid === 'resource_access') {
-        ret = field.value;
-      }
-    });
-    return ret;
-  };
-
-  var request_buffer = new Pride.Util.RequestBuffer({
-    url: getHoldingsUrl(data),
-    failure_message: Pride.Messenger.preset(
-      'failed_holdings_load',
-      data.names[0]
-    ),
-    edit_response: function(response) {
-      data = translateData(response);
-      return data;
-    }
-  });
-
-  var translateData = function(input) {
+  const translateData = function(input) {
     return [getResourceAccess(data)].concat(input);
   };
 
   this.getData = function(func) {
-    request_buffer.request({success: func});
+    Pride.Util.safeCall(func, translateData(this.data.holdings));
   };
-
 };
