15b96283c12b139699d3b6a679204435750bb6f2
Erin E. Sullivan <sulliv.erin@gmail.com>
2021-04-20 14:31:51 -0400

    Adding updated functionality

--- src/Pride/Core/Holdings.js
+++ src/Pride/Core/Holdings.js
@@ -1,48 +1,23 @@
 import { _ } from 'underscore';
-// import RequestBuffer from '../Util/RequestBuffer';
-// import Messenger from '../Messenger';
+import safeCall from '../Util/safeCall';
 
 const Holdings = function(data) {
   this.data = data;
 
-  const getHoldingsUrl = function(data) {
-    let ret;
-    _.each(data.fields, function(field) {
-      if (field.uid === 'holdings_url') {
-        ret = field.value;
-      }
-    });
-    return ret;
-  };
-
   const getResourceAccess = function(data) {
-    let ret;
-    _.each(data.fields, function(field) {
-      if (field.uid === 'resource_access') {
-        ret = field.value;
-      }
-    });
-    return ret;
-  };
-
-  const requestBuffer = new RequestBuffer({
-    url: getHoldingsUrl(data),
-    failure_message: Messenger.preset(
-      'failed_holdings_load',
-      data.names[0]
-    ),
-    edit_response: function(response) {
-      data = translateData(response);
-      return data;
+    const dataField = data.fields.find((field) => field.uid === 'resource_access');
+    if (dataField && dataField.value) {
+      return dataField.value;
     }
-  });
+    return dataField;
+  };
 
   const translateData = function(input) {
     return [getResourceAccess(data)].concat(input);
   };
 
   this.getData = function(func) {
-    requestBuffer.request({ success: func });
+    safeCall(func, translateData(this.data.holdings));
   };
 };
 
