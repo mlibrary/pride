4cc76f47f3b691e34b6877f45b57be44eb6e04fb
Erin E. Sullivan <sulliv.erin@gmail.com>
2021-04-28 14:21:53 -0400

    Adding unit tests

--- src/Pride/Core/Record.test.js
+++ src/Pride/Core/Record.test.js
@@ -1,9 +1,54 @@
+import { JSDOM } from 'jsdom';
 import { expect } from 'chai';
 import Record from './Record';
 
-describe.only('Record()', () => {
-  it('does a thing', () => {
-    console.log(Record);
+describe('Record()', () => {
+  beforeEach(() => {
+    const dom = new JSDOM();
+    global.window = dom.window;
+    global.document = dom.window.document;
+    this.data = {
+      source: 'https://lib.umich.edu/',
+      names: ['Homer', 'Marge', 'Bart', 'Lisa', 'Maggie']
+    };
+    this.recordExample = new Record(this.data);
+  });
+  it('works', () => {
     expect(Record).to.not.be.null;
+  });
+  describe('placeHold()', () => {
+    it('is a function', () => {
+      expect(this.recordExample.placeHold).to.be.a('function');
+    });
+  });
+  describe('getHoldings()', () => {
+    it('is a function', () => {
+      expect(this.recordExample.getHoldings).to.be.a('function');
+    });
+  });
+  describe('getGetThis()', () => {
+    it('is a function', () => {
+      expect(this.recordExample.getGetThis).to.be.a('function');
+    });
+  });
+  describe('renderPart()', () => {
+    it('is a function', () => {
+      expect(this.recordExample.renderPart).to.be.a('function');
+    });
+  });
+  describe('renderPartThenCache()', () => {
+    it('is a function', () => {
+      expect(this.recordExample.renderPartThenCache).to.be.a('function');
+    });
+  });
+  describe('renderFull()', () => {
+    it('is a function', () => {
+      expect(this.recordExample.renderFull).to.be.a('function');
+    });
+  });
+  describe('renderCSL()', () => {
+    it('is a function', () => {
+      expect(this.recordExample.renderCSL).to.be.a('function');
+    });
   });
 });
