8bfb97e096e87439d2cbb597f7af1f5ced65bb42
Erin E. Sullivan <sulliv.erin@gmail.com>
2021-05-03 10:58:48 -0400

    Pride.AllDatastores() unit test

--- src/Pride/requestRecord.test.js
+++ src/Pride/requestRecord.test.js
@@ -1,9 +1,11 @@
 import { expect } from 'chai';
 import requestRecord from './requestRecord';
 
-describe.only('requestRecord()', () => {
-  it('does a thing', () => {
-    console.log(requestRecord);
+describe('requestRecord()', () => {
+  it('works', () => {
     expect(requestRecord).to.not.be.null;
+  });
+  it('requires `source` argument', () => {
+    expect(() => requestRecord()).to.throw("Cannot read property 'get' of undefined");
   });
 });
