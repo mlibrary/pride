2ddf4a76faf3781f1b05368e6da2fbfb98d71928
Erin E. Sullivan <sulliv.erin@gmail.com>
2021-07-13 15:00:31 -0400

    Pride.init function

--- src/Pride/init.js
+++ src/Pride/init.js
@@ -1,34 +1,22 @@
-import { _ } from 'underscore';
-
-// import RequestBuffer from './Util/RequestBuffer';
-// import Settings from './Settings';
-// import Messenger from './Messenger';
-// import AllDatastores from './AllDatastores';
-// import Datastore from './Core/Datastore';
+import _ from 'underscore';
+import RequestBuffer from './Util/RequestBuffer';
+import Settings from './Settings';
+import Messenger from './Messenger';
+import AllDatastores from './AllDatastores';
+import Datastore from './Core/Datastore';
 
 const init = new RequestBuffer({
-  url: () => {
-    return Settings.datastoresUrl;
-  },
-  attempts: () => {
-    return Settings.initAttempts;
-  },
-  failure_message: () => {
-    return Messenger.preset('failed_init');
-  },
-
-  edit_response: () => {
-    return undefined;
-  },
+  url: () => Settings.datastores_url,
+  attempts: () => Settings.init_attempts,
+  failure_message: () => Messenger.preset('failed_init'),
+  edit_response: () => undefined,
   before_success: (data) => {
     // TODO: Look for a better place for this later.
-    Settings.defaultInstitution = data.defaultInstitution;
+    Settings.default_institution = data.default_institution;
     Settings.affiliation = data.affiliation;
     AllDatastores.array = _.map(
       data.response,
-      (datastoreData) => {
-        return new Datastore(datastoreData);
-      }
+      (datastoreData) => new Datastore(datastoreData)
     );
   }
 }).request;
