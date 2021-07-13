65c3731ac93cf5655dba5f852c3cb5fe79a9066f
Erin E. Sullivan <sulliv.erin@gmail.com>
2021-07-13 13:56:59 -0400

    Simplifying Pride.Settings tests

--- src/Pride/Settings.test.js
+++ src/Pride/Settings.test.js
@@ -9,27 +9,24 @@ describe('Settings', () => {
       });
     });
   });
-  describe('cache_size', () => {
-    it('returns an object', () => {
-      expect(Settings.cache_size).to.be.an('object');
+  ['cache_size', 'message_formats'].forEach((property) => {
+    describe(property, () => {
+      it('returns an object', () => {
+        expect(Settings[property]).to.be.an('object');
+      });
+      Object.keys(Settings[property]).forEach((key) => {
+        describe(key, () => {
+          it('returns a non-empty string', () => {
+            expect(Settings[property][key]).to.be.a('string');
+            expect(Settings[property][key]).to.not.be.empty;
+          });
+        });
+      });
     });
   });
   describe('datastores_url', () => {
     it('returns a string', () => {
       expect(Settings.datastores_url).to.be.a('string');
-    });
-  });
-  describe('message_formats', () => {
-    it('returns an object', () => {
-      expect(Settings.message_formats).to.be.an('object');
-    });
-    Object.keys(Settings.message_formats).forEach((key) => {
-      describe(key, () => {
-        it('returns a non-empty string', () => {
-          expect(Settings.message_formats[key]).to.be.a('string');
-          expect(Settings.message_formats[key]).to.not.be.empty;
-        });
-      });
     });
   });
   describe('obnoxious', () => {
