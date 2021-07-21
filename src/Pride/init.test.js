8f6c15c2b86e0bc4e9b8d352c9a0b23e8541b71a
Erin E. Sullivan <sulliv.erin@gmail.com>
2021-07-21 15:17:49 -0400

    Adding Pride.init unit tests

--- src/Pride/init.test.js
+++ src/Pride/init.test.js
@@ -1,9 +1,16 @@
 import { expect } from 'chai';
 import init from './init';
 
-describe.only('init()', () => {
-  it('does a thing', () => {
-    console.log(init);
+describe('init()', function() {
+  it('works', () => {
     expect(init).to.not.be.null;
+  });
+
+  it('is a function', () => {
+    expect(init).to.be.a('function');
+  });
+
+  it('requires the argument to have a defined `url` property', () => {
+    expect(() => init({})).to.throw('No URL given to request()');
   });
 });
