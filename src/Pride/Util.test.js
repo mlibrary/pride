5a7539caef85a3c277b2dbba207ee1469dada23f
Erin E. Sullivan <sulliv.erin@gmail.com>
2021-07-26 13:51:44 -0400

    Pride, Pride.Core, and Pride.Unit tests

--- src/Pride/Util.test.js
+++ src/Pride/Util.test.js
@@ -1,9 +1,21 @@
 import { expect } from 'chai';
 import Util from './Util';
 
-describe.only('Util()', () => {
-  it('does a thing', () => {
-    console.log(Util);
+describe('Util()', () => {
+  it('works', () => {
     expect(Util).to.not.be.null;
+  });
+  it('returns an object', () => {
+    expect(Util).to.be.an('object');
+  });
+  Object.getOwnPropertyNames(Util).forEach((property) => {
+    describe(`Util.${property}`, () => {
+      it('works', () => {
+        expect(Util[property]).to.not.be.null;
+      });
+      it('is a function', () => {
+        expect(Util[property]).to.be.a('function');
+      });
+    });
   });
 });
