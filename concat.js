502eeee0c55337b9b11b61f4fa740241a15d7c22
Erin E. Sullivan <sulliv.erin@gmail.com>
2023-10-03 15:48:51 -0400

    Add `esbuild` scripts, and adjust `concat.js` for building `pride.execjs.js`.

--- concat.js
+++ concat.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const glob = require('glob');
-
-const outputFile = './output.js';
+const execFile = process.env.BUNDLE === 'exec';
+const outputFile = execFile ? './output-exec.js' : './output.js';
 
 // Delete the output file if it exists
 fs.unlink(outputFile, function(err) {
@@ -16,10 +16,19 @@ glob('./source/*/**/*.js', {'ignore': ['./source/parser/**/*.js']}, function(err
       let editedDate = data;
       if (data.includes("import _ from 'underscore';")) {
         importUnderscore++;
+        // Remove importing `underscore` if already been done
+        if (importUnderscore > 1 || execFile) {
+          editedDate = data.replace("import _ from 'underscore';", '');
+        }
       }
-      // Remove importing `underscore` if already been done
-      if (importUnderscore > 1) {
-        editedDate = data.replace("import _ from 'underscore';", '');
+      // Replace certain strings for specs
+      if (execFile) {
+        if (data.includes("import reqwest from 'reqwest';")) {
+          editedDate = data.replace("import reqwest from 'reqwest';", '');
+        }
+        if (data.includes('export')) {
+          editedDate = data.replace('export', 'var reqwest = {};');
+        }
       }
       fs.appendFileSync(outputFile, editedDate);
     });
