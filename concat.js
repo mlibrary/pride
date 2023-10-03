54dc03f743b954af886d7fedfffc92f7ddefe939
Erin E. Sullivan <sulliv.erin@gmail.com>
2023-10-03 11:02:05 -0400

    Concatenate JavaScript files in a certain order. Ignore the output file.

--- concat.js
+++ concat.js
@@ -3,20 +3,21 @@ const glob = require('glob');
 
 const outputFile = './output.js';
 
-// delete output file if exists (otherwise it will continue appending)
+// Delete the output file if it exists
 fs.unlink(outputFile, function(err) {
   console.log(`${outputFile} has been removed`);
 });
 
-// concat all js files
-glob('./source/**/*.js', {'ignore': ['./source/parser/**/*.js']}, function(err, files) {
+// Concatenate all `./source` JavaScript files in a certain order, excluding files under `./source/parser`
+glob('./source/*/**/*.js', {'ignore': ['./source/parser/**/*.js']}, function(err, files) {
   let importUnderscore = 0;
-  files.forEach((file) => {
+  ['./source/initial_setup.js', './source/settings.js', ...files].forEach((file) => {
     fs.readFile(file, 'utf8', function(err, data){
       let editedDate = data;
       if (data.includes("import _ from 'underscore';")) {
         importUnderscore++;
       }
+      // Remove importing `underscore` if already been done
       if (importUnderscore > 1) {
         editedDate = data.replace("import _ from 'underscore';", '');
       }
