cee743490a3b372ca88786309acca1592699a017
Erin E. Sullivan <sulliv.erin@gmail.com>
2023-10-17 16:27:06 -0400

    Importing `Settings` as property.

--- pride.js
+++ pride.js
@@ -2456,6 +2456,51 @@ var PreferenceEngine = {
 };
 var PreferenceEngine_default = PreferenceEngine;
 
+// src/Pride/Settings.js
+var Settings = {
+  // default_cache_size:  If a cache size isn't set for a datastore, this value
+  //                      is used instead.
+  //
+  // cache_size:          Key-value pairs where each key is the UID of a
+  //                      datastore, and the value gives the cache size for that
+  //                      particular datastore.
+  //
+  // datastores_url:      URL from which Pride can get all the possible
+  //                      datastores.
+  //
+  // connection_attempts: How many times Pride will attempt an HTTP request
+  //                      before giving up (overridden by some things such as
+  //                      Pride.init()).
+  //
+  // init_attempts:       How many times Pride will attempt to initialize before
+  //                      giving up.
+  //
+  // ms_between_attempts: How long Pride will wait to try another HTTP request
+  //                      after one fails.
+  //
+  // message_formats:     Key-value pairs where each key is the ID of a message
+  //                      type and the value is what that message should say. A
+  //                      dollar sign preceded by a number will be replaced when
+  //                      the message is created.
+  //
+  // obnoxious:           If true, debug messages will be logged to the console
+  //                      as Pride runs. WARNING: Pride can send out a lot of
+  //                      debug messages.
+  default_cache_size: 20,
+  cache_size: {},
+  datastores_url: "",
+  connection_attempts: 3,
+  init_attempts: 3,
+  ms_between_attempts: 1500,
+  message_formats: {
+    failed_record_load: "Failed to load $1",
+    failed_search_run: "Failed to search $1",
+    failed_init: "Failed to initialize Pride"
+  },
+  obnoxious: false
+};
+var Settings_default = Settings;
+
 // src/Pride/Util/escape.js
 var escape = function(string) {
   var temp_element = document.createElement("div");
@@ -2504,7 +2549,8 @@ var slice_default = slice2;
 var import_reqwest = __toESM(require_reqwest());
 var Pride = {
   AllDatastores: AllDatastores_default,
-  PreferenceEngine: PreferenceEngine_default
+  PreferenceEngine: PreferenceEngine_default,
+  Settings: Settings_default
 };
 Pride.Util = {
   escape: escape_default2,
@@ -2513,48 +2559,6 @@ Pride.Util = {
   slice: slice_default
 };
 Pride.Core = {};
-Pride.Settings = {
-  // default_cache_size:  If a cache size isn't set for a datastore, this value
-  //                      is used instead.
-  //
-  // cache_size:          Key-value pairs where each key is the UID of a
-  //                      datastore, and the value gives the cache size for that
-  //                      particular datastore.
-  //
-  // datastores_url:      URL from which Pride can get all the possible
-  //                      datastores.
-  //
-  // connection_attempts: How many times Pride will attempt an HTTP request
-  //                      before giving up (overridden by some things such as
-  //                      Pride.init()).
-  //
-  // init_attempts:       How many times Pride will attempt to initialize before
-  //                      giving up.
-  //
-  // ms_between_attempts: How long Pride will wait to try another HTTP request
-  //                      after one fails.
-  //
-  // message_formats:     Key-value pairs where each key is the ID of a message
-  //                      type and the value is what that message should say. A
-  //                      dollar sign preceded by a number will be replaced when
-  //                      the message is created.
-  //
-  // obnoxious:           If true, debug messages will be logged to the console
-  //                      as Pride runs. WARNING: Pride can send out a lot of
-  //                      debug messages.
-  default_cache_size: 20,
-  cache_size: {},
-  datastores_url: "",
-  connection_attempts: 3,
-  init_attempts: 3,
-  ms_between_attempts: 1500,
-  message_formats: {
-    failed_record_load: "Failed to load $1",
-    failed_search_run: "Failed to search $1",
-    failed_init: "Failed to initialize Pride"
-  },
-  obnoxious: false
-};
 Pride.Core.Datastore = function(datastore_info) {
   datastore_info = Pride.Util.deepClone(datastore_info);
   this.baseQuery = function() {
--- source/initial_setup.js
+++ source/initial_setup.js
@@ -5,6 +5,7 @@
 
 import AllDatastores from './src/Pride/AllDatastores';
 import PreferenceEngine from './src/Pride/PreferenceEngine';
+import Settings from './src/Pride/Settings';
 
 import escape from './src/Pride/Util/escape';
 import isDeepMatch from './src/Pride/Util/isDeepMatch';
@@ -13,7 +14,8 @@ import slice from './src/Pride/Util/slice';
 
 export var Pride = {
   AllDatastores,
-  PreferenceEngine
+  PreferenceEngine,
+  Settings
 };
 
 Pride.Util = {
