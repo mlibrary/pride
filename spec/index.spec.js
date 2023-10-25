5cb07a7438cbc68c908eca65f532b2a7077fd8ab
Erin E. Sullivan <sulliv.erin@gmail.com>
2023-10-25 12:37:46 -0400

    Adding test to see if all `.js` files under `./src` have an associated `.spec.js` file under `./spec`.

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,13 +1,27 @@
+const fs = require('fs');
 const { expect } = require('chai');
-const Pride = require('../pride').Pride;
 
-describe('Pride', function () {
-  console.log('Pride', Object.keys(Pride));
-  console.log('Pride.Core', Object.keys(Pride.Core));
-  console.log('Pride.FieldTree', Object.keys(Pride.FieldTree));
-  console.log('Pride.Util', Object.keys(Pride.Util));
-  console.log('Pride.Util.Paginater', Object.keys(Pride.Util.Paginater));
-  it('works', function () {
-    expect(Pride).to.not.be.null;
+const deepSearch = (path = 'src') => {
+  fs.readdirSync(path).forEach((listing) => {
+    const listingPath = `${path}/${listing}`;
+    // Keep searching until the current listing is not a directory
+    if (!listing.includes('.')) {
+      deepSearch(listingPath);
+    }
+    // Check if current listing is a JavaScript file
+    if (listing.endsWith('.js')) {
+      // Convert current listing path into matching ./spec path
+      let specPath = path.split('/');
+      specPath[0] = 'spec';
+      specPath = `${specPath.join('/')}/${listing.slice(0, -3)}.spec.js`;
+      // Return test to see if the spec exists
+      it(`'./${listingPath}' has a spec file ('./${specPath}')`, () => {
+        expect(fs.existsSync(`${specPath}`)).to.be.true;
+      });
+    }
   });
+};
+
+describe('check if each `.js` file under `./src` has a `.spec.js` file under `./spec`', () => {
+  deepSearch();
 });
