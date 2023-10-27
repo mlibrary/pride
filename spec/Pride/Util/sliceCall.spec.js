af59f89cae1f8ad1b8d8f2493abbd4dd6639fd91
Erin E. Sullivan <sulliv.erin@gmail.com>
2023-10-27 15:23:23 -0400

    Using DRY principle.

--- spec/Pride/Util/sliceCall.spec.js
+++ spec/Pride/Util/sliceCall.spec.js
@@ -3,28 +3,23 @@ const sliceCall = require('../../../pride').Pride.Util.sliceCall;
 
 describe('Pride.Util.sliceCall()', function () {
   const args = [1, 2, 3, 4];
-
-  describe('slicing arrays', function () {
+  const sliceTests = (array = args) => {
     it('slices from start', function () {
-      expect(sliceCall(args, 1)).to.deep.equal([2, 3, 4]);
+      expect(sliceCall(array, 1)).to.deep.equal([2, 3, 4]);
     });
 
     it('slices to end', function () {
-      expect(sliceCall(args, 1, 3)).to.deep.equal([2, 3]);
+      expect(sliceCall(array, 1, 3)).to.deep.equal([2, 3]);
     });
+  };
+
+  describe('slicing arrays', function () {
+    sliceTests();
   });
 
   describe('slicing array-like objects', function () {
-    const arrayLike = (function () {
+    sliceTests((function () {
       return arguments;
-    }(...args));
-
-    it('slices from start', function () {
-      expect(sliceCall(arrayLike, 1)).to.deep.equal([2, 3, 4]);
-    });
-
-    it('slices to end', function () {
-      expect(sliceCall(arrayLike, 1, 3)).to.deep.equal([2, 3]);
-    });
+    }(...args)));
   });
 });
