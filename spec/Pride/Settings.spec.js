96547249ab0f93d73a93acdf6cbbf70de258e188
Erin E. Sullivan <sulliv.erin@gmail.com>
2023-10-31 09:00:39 -0400

    Cleaning up `Pride.Settings` and writing tests.

--- spec/Pride/Settings.spec.js
+++ spec/Pride/Settings.spec.js
@@ -1,8 +1,31 @@
 const { expect } = require('chai');
-const Pride = require('../../pride').Pride;
+const Settings = require('../../pride').Pride.Settings;
 
 describe('Pride.Settings', function () {
   it('works', function () {
-    expect(Pride.Settings).to.not.be.null;
+    expect(Settings).to.not.be.null;
+  });
+  it('is a non-empty object', function () {
+    expect(Settings).to.be.an('object');
+    expect(Settings).to.not.be.empty;
+  });
+  describe('properties', function () {
+    it('Settings.cache_size returns an object', function () {
+      expect(Settings.cache_size).to.be.an('object');
+    });
+    ['connection_attempts', 'default_cache_size', 'init_attempts', 'ms_between_attempts'].forEach((property) => {
+      it(`Settings.${property} returns a number`, function () {
+        expect(Settings[property]).to.be.a('number');
+      });
+    });
+    ['datastores_url', 'message_formats.failed_init', 'message_formats.failed_record_load', 'message_formats.failed_search_run'].forEach((property) => {
+      it(`Settings.${property} returns a string`, function () {
+        property = property.split('.');
+        expect(property.includes('message_formats') ? Settings.message_formats[property.pop()] : Settings[property.pop()]).to.be.a('string');
+      });
+    });
+    it('Settings.obnoxious returns a boolean', function () {
+      expect(Settings.obnoxious).to.be.a('boolean');
+    });
   });
 });
--- src/Pride/Settings.js
+++ src/Pride/Settings.js
@@ -1,50 +1,46 @@
+/*
+  cache_size:          Key-value pairs where each key is the UID of a
+                       datastore, and the value gives the cache size for that
+                       particular datastore.
+
+  connection_attempts: How many times Pride will attempt an HTTP request
+                       before giving up (overridden by some things such as
+                       Pride.init()).
+
+  datastores_url:      URL from which Pride can get all the possible
+                       datastores.
+
+  default_cache_size:  If a cache size isn't set for a datastore, this value
+                       is used instead.
+
+  init_attempts:       How many times Pride will attempt to initialize before
+                       giving up.
+
+  message_formats:     Key-value pairs where each key is the ID of a message
+                       type and the value is what that message should say. A
+                       dollar sign preceded by a number will be replaced when
+                       the message is created.
+
+  ms_between_attempts: How long Pride will wait to try another HTTP request
+                       after one fails.
+
+  obnoxious:           If true, debug messages will be logged to the console
+                       as Pride runs. WARNING: Pride can send out a lot of
+                       debug messages.
+*/
+
 const Settings = {
-  // default_cache_size:  If a cache size isn't set for a datastore, this value
-  //                      is used instead.
-  //
-  // cache_size:          Key-value pairs where each key is the UID of a
-  //                      datastore, and the value gives the cache size for that
-  //                      particular datastore.
-  //
-  // datastores_url:      URL from which Pride can get all the possible
-  //                      datastores.
-  //
-  // connection_attempts: How many times Pride will attempt an HTTP request
-  //                      before giving up (overridden by some things such as
-  //                      Pride.init()).
-  //
-  // init_attempts:       How many times Pride will attempt to initialize before
-  //                      giving up.
-  //
-  // ms_between_attempts: How long Pride will wait to try another HTTP request
-  //                      after one fails.
-  //
-  // message_formats:     Key-value pairs where each key is the ID of a message
-  //                      type and the value is what that message should say. A
-  //                      dollar sign preceded by a number will be replaced when
-  //                      the message is created.
-  //
-  // obnoxious:           If true, debug messages will be logged to the console
-  //                      as Pride runs. WARNING: Pride can send out a lot of
-  //                      debug messages.
-
-  default_cache_size: 20,
-  cache_size: {
-
-  },
-
-  datastores_url: '',
-
+  cache_size: {},
   connection_attempts: 3,
+  datastores_url: '',
+  default_cache_size: 20,
   init_attempts: 3,
-  ms_between_attempts: 1500,
-
   message_formats: {
+    failed_init: 'Failed to initialize Pride',
     failed_record_load: 'Failed to load $1',
-    failed_search_run: 'Failed to search $1',
-    failed_init: 'Failed to initialize Pride'
+    failed_search_run: 'Failed to search $1'
   },
-
+  ms_between_attempts: 1500,
   obnoxious: false
 };
 
