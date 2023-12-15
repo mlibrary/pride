1fc84c0202151a2cd01d8d34a8d18c53a416ed6f
Erin E. Sullivan <sulliv.erin@gmail.com>
2023-12-15 09:47:29 -0500

    Adding tests for `type`, `childTypes`, and `extension`.

--- spec/Pride/FieldTree/Field.spec.js
+++ spec/Pride/FieldTree/Field.spec.js
@@ -1,8 +1,51 @@
 const { expect } = require('chai');
-const Pride = require('../../../pride').Pride;
+const Field = require('../../../pride').Pride.FieldTree.Field;
 
 describe('Pride.FieldTree.Field', function () {
+  const testField = 'testField';
+  const childField = 'childField';
+  let createField;
+  let createChildField;
+
+  beforeEach(function () {
+    createField = function () {
+      return new Field(testField, ...arguments);
+    };
+    createChildField = new Field(childField);
+  });
+
   it('works', function () {
-    expect(Pride.FieldTree.Field).to.not.be.null;
+    expect(Field).to.not.be.null;
+  });
+
+  describe('type', function () {
+    it('defines the type via a string', function () {
+      expect(createField().type).to.be.a('string');
+      expect(createField().type).deep.equal('field');
+    });
+  });
+
+  describe('childTypes', function () {
+    it('contains an array of specific values', function () {
+      expect(createField().childTypes).to.be.an('array');
+      expect(createField().childTypes).deep.equal(['value_boolean', 'literal', 'tag', 'special']);
+    });
+  });
+
+  describe('extension', function () {
+    it('serializes correctly with no children', function () {
+      const field = createField();
+      expect(field.serialize()).to.equal(`${testField}: ()`);
+    });
+
+    it('serializes correctly with one child', function () {
+      const field = createField(createChildField);
+      expect(field.serialize()).to.equal(`${testField}: (${childField}: ())`);
+    });
+
+    it('serializes correctly with multiple children', function () {
+      const field = createField(createChildField, createChildField);
+      expect(field.serialize()).to.equal(`${testField}: (${childField}: () ${childField}: ())`);
+    });
   });
 });
