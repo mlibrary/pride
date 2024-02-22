56383a1971e5131636bf7bc3612893e287bd9d28
Erin E. Sullivan <sulliv.erin@gmail.com>
2024-02-22 14:04:53 -0500

    Converting `Pride.FieldTree.Tag` to ECMAScript 2021.

--- src/Pride/FieldTree/Tag.js
+++ src/Pride/FieldTree/Tag.js
@@ -1,14 +1,10 @@
 import nodeFactory from '../Core/nodeFactory';
 import insideFieldNodes from './insideFieldNodes';
 
-const Tag = nodeFactory(
-  'tag',
-  insideFieldNodes,
-  function () {
-    this.serialize = function () {
-      return `${this.value}: (${this.serializedChildren().join(' ')})`;
-    };
-  }
-);
+const Tag = nodeFactory('tag', insideFieldNodes, function () {
+  this.serialize = () => {
+    return `${this.value}: (${this.serializedChildren().join(' ')})`;
+  };
+});
 
 export default Tag;
