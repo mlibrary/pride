fdb0728543b9da52a3a3880dbe413aac9871a37a
Erin E. Sullivan <sulliv.erin@gmail.com>
2024-02-28 12:00:59 -0500

    Removing the need to copy over logic from `Pride.Util.escape`.

--- spec/Pride/Util/escape.spec.js
+++ spec/Pride/Util/escape.spec.js
@@ -1,30 +1,31 @@
 const { expect } = require('chai');
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
-const { document } = (new JSDOM()).window;
-
-const symbols = [
-  {
-    symbol: '&',
-    html: '&amp;'
-  },
-  {
-    symbol: '<',
-    html: '&lt;'
-  },
-  {
-    symbol: '>',
-    html: '&gt;'
-  }
-];
+const escape = require('../../../pride').Pride.Util.escape;
+const { window } = new JSDOM('');
+global.document = window.document;
 
 describe('Pride.Util.escape()', function () {
-  symbols.forEach((symbol) => {
-    it(`encodes ${symbol.symbol} properly`, function () {
-      // Copy code from ./src/Pride/Util/escape.js to define `document`
-      const tempElement = document.createElement('div');
-      tempElement.appendChild(document.createTextNode(symbol.symbol));
-      expect(tempElement.innerHTML).to.equal(symbol.html);
+  it('works', function () {
+    expect(escape).to.not.be.null;
+  });
+
+  [
+    {
+      symbol: '&',
+      html: '&amp;'
+    },
+    {
+      symbol: '<',
+      html: '&lt;'
+    },
+    {
+      symbol: '>',
+      html: '&gt;'
+    }
+  ].forEach((character) => {
+    it(`encodes ${character.symbol} properly`, function () {
+      expect(escape(character.symbol)).to.equal(character.html);
     });
   });
 });
