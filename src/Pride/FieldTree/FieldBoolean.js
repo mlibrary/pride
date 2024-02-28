e698858f1a52e494ddbbc41e9e13ab168be3eaa1
Erin E. Sullivan <sulliv.erin@gmail.com>
2024-02-28 10:16:35 -0500

    Removing `Pride.Core.boolNodeFactory` as logic has been moved to `Pride.FieldTree.FieldBoolean`.

--- src/Pride/FieldTree/FieldBoolean.js
+++ src/Pride/FieldTree/FieldBoolean.js
@@ -1,5 +1,26 @@
-import boolNodeFactory from '../Core/boolNodeFactory';
+import nodeFactory from '../Core/nodeFactory';
 
-const FieldBoolean = boolNodeFactory('field_boolean', ['field_boolean', 'field']);
+const FieldBoolean = nodeFactory(
+  'field_boolean',
+  ['field_boolean', 'field'],
+  function () {
+    if (!['AND', 'OR', 'NOT'].includes(this.value)) {
+      throw new Error('Not a valid boolean value');
+    }
+
+    this.serializedChildren = () => {
+      return this.children.map((child) => {
+        if (child.type === this.type || (child.type === 'literal' && child.value.match(/\s/))) {
+          return `(${child.serialize()})`;
+        }
+        return child.serialize();
+      });
+    };
+
+    this.serialize = () => {
+      return this.serializedChildren().join(` ${this.value} `);
+    };
+  }
+);
 
 export default FieldBoolean;
